29ab9ebf48ad4233e97512e3db00eb6c8cd30dad
Amanda Sengchiam <amsengchiam@gmail.com>
2022-02-10 14:36:52 -0600

    added get and post route to primaryData routes

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,8 +1,9 @@
-const uuid = require('uuid');
-const mongoose = require('mongoose');
+const uuid = require('uuid'); //uuid will allow us to store the _id as a string
+const mongoose = require('mongoose'); //mongoose will be our ODM
 const Schema = mongoose.Schema;
 
 //collection for applicants
+
 let primaryDataSchema = new Schema({
     _id: { type: String, default: uuid.v1 },
     firstName: {
@@ -33,5 +34,5 @@ let primaryDataSchema = new Schema({
 //create models for mongoose schema to use primaryData and commonData data model ('js', schema name)
 const primarydata = mongoose.model('primaryData', primaryDataSchema); 
 
-// package the models in an object to export both
+// package the models in an object to export 
 module.exports = {primarydata}
--- backend/routes/primaryData.js
+++ backend/routes/primaryData.js
@@ -1,24 +1,65 @@
-const express = require("express");
-const { validate } = require("uuid");
-const router = express.Router();
+const express = require("express"); //import express
+const {validate} = require("uuid"); // uuid lets us use the _id as a string
+const router = express.Router(); //creates an instance of the route handler
 
-//importing data model schema's
-let { primarydata } = require("../models/models");
+//importing data model schemas
+let {primarydata} = require("../models/models"); //imports the primarydata model from models.js}
 
-// gets all of primaryData from db
-router.get("/", (req, res, next) => {
-    primarydata.find(
-      {},
-      (error, data) => {
-        // error handler
-        if (error) {
-          return next(error);
-        } else {
-          res.json(data);
+//GET host:port/primaryData || this endpoint will use GET method to return all entries from primaryData collection, 
+router.get("/", (req, res, next) => { // route method, path, followed by callback
+    primarydata.find( //model.find({filters}) finds documents, since we have no filters, it returns everything
+        (error, data) => { // error handler
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data); // returns the response data as json
+            }
         }
-      }
     );
-  });
+});
 
-// exports the router
+//GET host:port/primaryData/:id || this endpoint will use GET method to return a doucment from primaryData collection, using :id as a parameter 
+router.get("/:id", (req, res, next) => { // route method, path followed by a parameter, callback
+  primarydata.findOne( //model.find({filter}) finds documents based on the {filter}, can have many as long as they are seperated by comma.
+    { _id: req.params.id}, // // our first filter is to find document where _id matches the :id from our endpoint. ex: host:port/primaryData/12345 12345 is our req.params.id
+      (error, data) => { // error handler
+          if (error) {
+              return next(error);
+          } else {
+              res.json(data); // returns the response data as json
+          }
+      }
+  );
+});
+
+//POST: host:port/primaryData || this endpoint will use POST method to create document for primaryData collection,  
+router.post("/", (req, res, next) => { // route method, path, callback 
+  primarydata.create( //model.create() creates a new document using primarydata model
+    req.body, //use the entire request body as the data for our new document: primarydata.create (req.body)
+      (error, data) => { // error handler 
+          if (error) {
+              return next(error);//
+          } else {
+              res.json(data); // returns the response data as json
+          }
+      }
+  );
+});
+
+//PUT: host:port/primaryData || this endpoint will use PUT method to update an existing document, using :id as a parameter in primaryData collection,  
+/* router.put("/:id", (req, res, next) => { // route method, path followed by parameter, callback 
+  primarydata.findOneAndUpdate( //model.findOneAndUpdate({filter}) finds and updates a document based on what arguements you pass. filters are contained within brackets 
+    { _id: req.params.id }, // our first filter is to find document where _id matches the :id from our endpoint. ex: host:port/primaryData/12345 12345 is our req.params.id
+    req.body, //use the entire request body as the data for updated document. you can also do {$set: {field: req.body.matchingfield}} to updated only a set of specific properties.
+      (error, data) => { // error handler 
+          if (error) {
+              return next(error);//
+          } else {
+              res.send('Updated the Primary Data for client'); // returns a message as a response
+          }
+      }
+  );
+}); */
+
+// exports the router module so we can load it in app.js
 module.exports = router;
