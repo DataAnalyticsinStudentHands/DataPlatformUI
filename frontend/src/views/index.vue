e2fe78c49d81d3a0ac27749c0e2bff1d25cc65b7
severinkrystyan <krystyanseverin@gmail.com>
2022-03-20 20:21:22 -0500

    Created new view called index

---
+++ frontend/src/views/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <dashboard />
+</template>
+<script>
+import Dashboard from "../components/dashboard.vue";
+export default {
+  name: "App",
+  components: {
+    Dashboard,
+  },
+};
+</script>
+<style>
+</style>
