edd9db1fc37fd196329412c62613bfd5d10cd14a
severinkrystyan <krystyanseverin@gmail.com>
2022-03-20 20:17:31 -0500

    Created VUEX Store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,31 @@
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import router from './router'
 import App from './App.vue'
 import './assets/tailwind.css'
 
+//Vuex store
+const store = createStore({
+    state() {
+        return {
+            windowWidth: 9999,
+            isResponsive: false
+        }
+    },
+    mutations: {
+        updateWindowWidth(state, width) {
+            state.windowWidth = width;
+            if (width < 900) {
+                state.isResponsive = true;
+            } else {
+                state.isResponsive = false;
+            }
+        }
+
+    }
+})
+
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount('#app');
