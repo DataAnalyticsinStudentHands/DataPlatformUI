2a9e99691c4b5e602b8a3cf1c70ba1ef2072fe8c
severinkrystyan <krystyanseverin@gmail.com>
2022-04-01 13:27:12 -0500

    Edited endpoint for get query

--- backend/routes/eventsData.js
+++ backend/routes/eventsData.js
@@ -19,12 +19,12 @@ router.get("/", (req, res, next) => { // route method, path, followed by callbac
 });
 
 //GET host:port/primaryData/:id || this endpoint will use GET method to return a doucment from primaryData collection, using :id as a parameter 
-router.get("/:id", (req, res, next) => { // route method, path followed by a parameter, callback
-    eventdata.findOne({_id: req.params.id},(error, data) => {
+router.get("/eventdetails/", (req, res, next) => { // route method, path followed by a parameter, callback
+    eventdata.find({ _id: req.query.id }, (error, data) => {
         if (error) {
-          return next(error)
+            return next(error)
         } else {
-          res.json(data)
+            res.json(data)
         }
     })
 });
@@ -47,6 +47,7 @@ router.post("/", (req, res, next) => { // route method, path, callback
 //PUT: host:port/commonData || this endpoint will use PUT method to update an existing document, using :id as a parameter in primaryData collection,  
 //you can pass the existing document as your req.data, just be sure to exclude the _id field and _id value.
 router.put("/:id", (req, res, next) => { // route method, path followed by parameter, callback 
+    console.log(req.body)
     eventdata.findOneAndUpdate( //model.findOneAndUpdate({filter}) finds and updates a document based on what arguements you pass. filters are contained within brackets 
         { _id: req.params.id }, // our first filter is to find document where _id matches the :id from our endpoint. ex: host:port/primaryData/12345 12345 is our req.params.id
         req.body, //use the entire request body as the data for updated document. you can also do {$set: {field: req.body.matchingfield}} to updated only a set of specific properties.
