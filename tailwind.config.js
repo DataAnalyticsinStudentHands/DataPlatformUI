3cd8155088572af4077afdedf20f7eef964f81e1
Michael Summers <msasummers@yahoo.com>
2022-06-29 22:31:52 -0500

    create tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ['./frontend/src/**/*/.{vue,html,js}' ],
+  darkMode: false,
+  theme: {
+    colors: {
+      'red': '#C8102E',
+      'brick': '#960C22',
+      'chocolate': '#640817',
+      'teal': '#640817',
+      'green': '#00866C',
+      'forest': '#005950',
+      'gold': '#F6BE00',
+      'mustard': '#D89B00',
+      'ocher': '#B97800',
+      'gray': '#888B8D',
+      'slate': '#54585A',
+      'black': '#000000',
+      'cream': '#FFF9D9',
+      'white': '#FFFFFF',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
