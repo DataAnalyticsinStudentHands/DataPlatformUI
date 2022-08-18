1a35ba015b0511ff65e84d8ff405a96e596202de
Michael Summers <msasummers@yahoo.com>
2022-08-18 14:11:55 -0500

    routing

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,7 +10,7 @@ const store = createStore({
         return {
             windowWidth: 9999,
             isResponsive: false,
-            ipAddress: '192.168.0.69'
+            ipAddress: '172.25.114.93'
         }
     },
     mutations: {
