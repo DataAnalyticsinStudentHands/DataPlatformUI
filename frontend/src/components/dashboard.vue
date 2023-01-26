a618ea28051c502214b705458333e6a8962d3ecf
lorivo <lorivvo2gmail.com>
2023-01-26 16:16:43 -0600

    recent events on dashboard

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -6,15 +6,40 @@
       >
         Welcome {{ firstName }} {{ lastName }}
       </h1>
+      <br>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
+      <div class="ml-10">
+      </div>
+      <div class="flex flex-col col-span-2">
+        <table class="min-w-full shadow-md rounded">
+          <thead class="bg-gray-50 text-xl">
+            <tr>
+              <th class="p-4 text-left">Event Name</th>
+              <th class="p-4 text-left">Event Date</th>
+              <th class="p-4 text-left">Attendees Amount</th>
+            </tr>
+          </thead>
+          <tbody class="divide-y divide-gray-300">
+            <tr v-for="event in queryData" :key="event._id">
+              <td class="p-2 text-left">{{ event.eventName }}</td>
+              <td class="p-2 text-left">{{ formattedDate(event.date) }}</td>
+              <td class="p-2 text-left">{{ event.attendees }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
     </div>
   </main>
 </template>
 <script>
+import { DateTime } from "luxon";
 import axios from "axios";
 
 export default {
   data() {
     return {
+      queryData: [],
       firstName: "",
       lastName: "",
     };
@@ -41,11 +66,19 @@ export default {
             this.$router.push("/login");
           }
         }
-      );
+      )
+    let apiURL = import.meta.env.VITE_ROOT_API + `/dashboarddata/recentEvent/`;
+    this.queryData = [];
+    axios.get(apiURL).then((resp) => {
+      this.queryData = resp.data;
+    });
   },
   methods: {
     routePush(routeName) {
       this.$router.push({ name: routeName });
+    },
+    formattedDate(datetimeDB) {
+      return DateTime.fromISO(datetimeDB).plus({ days: 1 }).toLocaleString();
     },
   },
 };
