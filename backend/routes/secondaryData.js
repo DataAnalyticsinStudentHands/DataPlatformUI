26ea3cb6b3b42b77f278de12de0e093a9d235219
schoolassignedAG <aagarc35@cougarnet.uh.edu>
2023-01-26 17:21:50 -0600

    POST for secondaryData

--- backend/routes/secondaryData.js
+++ backend/routes/secondaryData.js
@@ -18,21 +18,45 @@ router.get("/id/:id", (req, res, next) => {
     );
 });
 
-//PUT update (make sure req body doesn't have the id)
-router.put("/:id", (req, res, next) => { 
+//POST
+router.post("/", (req, res, next) => {
     // always use orgID from instance
     req.body.organizationID = orgID;
-    secondarydata.findOneAndUpdate( 
-        { _id: req.params.id }, 
-        req.body,
-        (error, data) => {
-            if (error) {
-                return next(error);
-            } else {
-                res.json(data);
-            }
+    
+    //only add primarydata if searchParam is not in system
+    secondarydata.findOne({ "clientID": req.body.clientID, organizationID: orgID }, (err, returndata) => {
+        if(err) {
+            return res.status(500).json({
+            title: 'server error',
+            error: err})
         }
-    );
+        if (returndata) {
+            secondarydata.findOne({ "clientID": req.body.clientID }, (err, returndata2) => {
+                if(returndata2) {
+                    return res.status(401).json({
+                        title: "Has Existing Secondary Information",
+                        error: 'Client has exists this information already associated in this organization.',
+                        data: returndata2
+                    })
+                }
+            })
+        }
+        else if (returndata == null) {
+            secondarydata.create( 
+                req.body,
+                (error, data) => { 
+                    if (error) {
+                        return next(error);
+                    } else {
+                        return res.json(data);
+                    }
+                }
+            );
+            secondarydata.createdAt;
+            secondarydata.updatedAt;
+            secondarydata.createdAt instanceof Date;
+        }
+    });
 });
 
 module.exports = router;
