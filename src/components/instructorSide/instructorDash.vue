85c0c4b0ed0705b4a0aecb4f1b560cf7b76aee34
lorivo <lorivvo2gmail.com>
2023-08-02 12:39:05 -0500

    removed irrelevant text

--- src/components/instructorSide/instructorDash.vue
+++ src/components/instructorSide/instructorDash.vue
@@ -5,7 +5,7 @@
       <h1
         class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
       >
-        Welcome {{ firstName }} {{ lastName }} {{ this.role }} asdasd {{  res}}
+        Welcome {{ firstName }} {{ lastName }} {{ this.role }}  {{  res}}
       </h1>
       <br>
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
