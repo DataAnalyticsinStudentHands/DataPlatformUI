b8d4fbfae4bbf8b35eacea0a7e7384a11f536635
lorivo <lorivvo2gmail.com>
2023-08-11 17:00:16 -0500

    added cvs for all goal setting forms and table for experience and studnet count

--- src/components/instructorSide/instructorReports.vue
+++ src/components/instructorSide/instructorReports.vue
@@ -5,7 +5,24 @@
     <v-btn @click="downloadEntryDataAsCSV">Download Student Entry Forms as CSV</v-btn>
     <br>
     <br>
+    <v-btn @click="downloadAllGoalDataAsCSV">Download Student Goal Setting Forms as CSV for <b>All</b> Semesters</v-btn>
+    <br>
+    <br>
     <v-btn @click="downloadAllExitDataAsCSV">Download Student Exit Forms as CSV for <b>All</b> Semesters</v-btn>
+    <table class="min-w-full shadow-md rounded">
+          <thead class="bg-gray-50 text-xl">
+            <tr>
+              <th class="p-4 text-left">Experience Name</th>
+              <th class="p-4 text-left">Student Amount</th>
+            </tr>
+          </thead>
+          <tbody class="divide-y divide-gray-300">
+            <tr v-for="experience in queryData" :key="experience.experienceID">
+              <td class="p-2 text-left">{{experience.experienceCategory}}: {{ experience.experienceName }}</td>
+              <td class="p-2 text-left">{{ experience.count }}</td>
+            </tr>
+          </tbody>
+        </table>
   </div>
 </template>
 
@@ -14,6 +31,24 @@ import axios from 'axios';
 import { useLoggedInUserStore } from "@/stored/loggedInUser";
 
 export default {
+  data(){
+    return{
+    queryData:[],
+    }
+  },
+  mounted(){
+    const user = useLoggedInUserStore()
+    let token = user.token
+    let apiURL = import.meta.env.VITE_ROOT_API + `/studentSideData/getExperienceCount/`;
+    this.queryData = [];
+    console.log(this.queryData);
+    axios.get(apiURL,{
+        headers: { token },
+      }).then((resp) => {
+      this.queryData = resp.data;
+    console.log(resp.data);
+    });
+  },
   methods: {
     async downloadEntryDataAsCSV() {
       try {
@@ -51,6 +86,36 @@ export default {
         let token = user.token;
         // Fetch JSON data from the API endpoint
         const response = await axios.get(import.meta.env.VITE_ROOT_API +'/studentSideData/exitForms/all', { headers: { token } });
+        const jsonData = response.data;
+
+        // Convert JSON to CSV format
+        const csvData = this.convertToCSV(jsonData);
+
+        // Create a Blob containing the CSV data
+        const blob = new Blob([csvData], { type: 'text/csv' });
+
+        // Create a download link and trigger the download
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'data.csv';
+        document.body.appendChild(link);
+        link.click();
+
+        // Clean up
+        URL.revokeObjectURL(url);
+        document.body.removeChild(link);
+      } catch (error) {
+        console.error('Error downloading data:', error);
+      }
+    },
+
+    async downloadAllGoalDataAsCSV() {
+      try {
+        const user = useLoggedInUserStore();
+        let token = user.token;
+        // Fetch JSON data from the API endpoint
+        const response = await axios.get(import.meta.env.VITE_ROOT_API +'/studentSideData/goalForm/all/', { headers: { token } });
         const jsonData = response.data;
 
         // Convert JSON to CSV format
