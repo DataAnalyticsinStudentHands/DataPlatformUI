082556962e0ca198b5106dde7f9eb6898ed6f01a
erikssonphil <paeriksson@uh.edu>
2023-08-24 19:55:31 -0500

    Building Reports

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,20 +1,71 @@
 <template>
   <v-container>
     <!-- Filters section -->
-    <v-row class="mb-5" style="background-color: #f5f5f5;">
+    <v-row class="mb-0 pa-0" style="background-color: #f5f5f5;">
       <v-col cols="12">
-        <!-- Filters go here -->
-        Filters
+        <v-row no-gutters>
+          <v-col cols="auto" class="d-flex align-center">
+            <v-checkbox
+              v-model="uhStudentsFilterEnabled"
+              hide-details
+              density="compact"
+            ></v-checkbox>
+          </v-col>
+          <v-col cols="2" class="d-flex align-center pa-2">
+            <p :class="{ 'gray-out': !uhStudentsFilterEnabled }">{{ uhStudentsValue ? 'UH Students Only' : 'Non-UH Students Only' }}</p>
+          </v-col>
+          <v-col cols="auto" class="d-flex align-center">
+            <v-switch
+              v-model="uhStudentsValue"
+              :disabled="!uhStudentsFilterEnabled"
+              :true-label="'UH Students Only'"
+              :false-label="'Non-UH Students Only'"
+              hide-details
+              density="compact"
+            ></v-switch>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+
+    <!-- New filters section -->
+    <v-row class="mb-0 pa-0" style="background-color: #f5f5f5;">
+      <v-col cols="12">
+        <v-row no-gutters>
+          <v-col cols="auto" class="d-flex align-center">
+            <!-- New checkbox -->
+            <v-checkbox
+              v-model="graduateFilterEnabled"
+              hide-details
+              density="compact"
+            ></v-checkbox>
+          </v-col>
+          <v-col cols="4" class="d-flex align-center pa-2">
+            <!-- New filter text -->
+            <p :class="{ 'gray-out': !graduateFilterEnabled }">{{ pursuingGraduateValue ? 'Pursuing Graduate/Professional School' : 'Not Pursuing Graduate/Professional School' }}</p>
+          </v-col>
+          <v-col cols="auto" class="d-flex align-center">
+            <!-- New switch -->
+            <v-switch
+              v-model="pursuingGraduateValue"
+              :disabled="!graduateFilterEnabled"
+              :true-label="'Pursuing Graduate/Professional School'"
+              :false-label="'Not Pursuing Graduate/Professional School'"
+              hide-details
+              density="compact"
+            ></v-switch>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
 
     <!-- Bar charts sections -->
     <v-row>
-      <v-col cols="6" style="background-color: #d9d9d9;">
+      <v-col cols="6" class="pa-0">
         <!-- Left side content -->
         Left Bar Chart
       </v-col>
-      <v-col cols="6" style="background-color: #b0b0b0;">
+      <v-col cols="6" class="pa-0">
         <!-- Right side content -->
         Right Bar Chart
       </v-col>
@@ -24,9 +75,20 @@
 
 <script>
 export default {
-  name: 'ReportsPage'
+  name: 'ReportsPage',
+  data() {
+    return {
+      uhStudentsFilterEnabled: false,
+      uhStudentsValue: false,
+      graduateFilterEnabled: false,
+      pursuingGraduateValue: false
+    };
+  }
 }
 </script>
 
 <style scoped>
+.gray-out {
+  color: #aaa; /* Use the desired gray color */
+}
 </style>
