10503d241dc05654c1a7819372fef20206fb2b15
Philip Eriksson <erikssonphil@yahoo.com>
2024-03-26 22:15:50 -0500

    automatic mailer goal forms

--- src/components/instructorSide/instructorMailer/instructorAutomaticMailer/instructorAutomaticMailerEntryForms.vue
+++ src/components/instructorSide/instructorMailer/instructorAutomaticMailer/instructorAutomaticMailerEntryForms.vue
@@ -21,45 +21,151 @@
                 ></v-switch>
             </v-col>
         </v-row>
+        <div v-if="entryFormEmail.enabled">
+            <v-row>
+                <v-col cols="6">
+                    <v-text-field
+                        v-model="entryFormEmail.firstEmailDays"
+                        label="First Email Days"
+                        type="number"
+                        min="0"
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                    <v-text-field
+                        v-model="entryFormEmail.secondEmailDays"
+                        label="Second Email Days"
+                        type="number"
+                        min="0"
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-checkbox
+                        v-model="entryFormEmail.multilingual"
+                        label="Include Translation"
+                        hide-details    
+                    ></v-checkbox>
+                    <div class="text-subtitle-2 ml-4">Write a message in English and Spanish. Students will receive one based on their "Language Preference"</div>
+                </v-col>
+            </v-row>
+            <div v-if="!entryFormEmail.multilingual">
+                <v-row>
+                    <v-col class="d-flex text-h6">
+                        Email
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12">
+                        <v-text-field 
+                            v-model="entryFormEmail.emailSubject"
+                            label="Subject Header" 
+                            hide-details
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12">
+                        <ckeditor :editor="editor" v-model="entryFormEmail.emailContent" :config="editorConfig" @ready="onEditorReady"></ckeditor>
+                    </v-col>
+                </v-row>
+                <v-row>
+                <v-col cols="12">
+                    <v-btn @click="addStudentName">Add Student Name</v-btn>
+                </v-col>
+            </v-row>
+            </div>
+            <div v-else>
+                <v-row>
+                    <v-col cols="6">
+                        <v-row>
+                            <v-col class="d-flex text-h6">
+                                English Email
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field 
+                                    v-model="entryFormEmail.emailSubjectEnglish"
+                                    label="Subject Header" 
+                                    hide-details
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <ckeditor :editor="editor" v-model="entryFormEmail.emailContentEnglish" :config="editorConfig" @ready="onEditorEnglishReady"></ckeditor>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-btn @click="addStudentNameEnglish">Add Student Name</v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-row>
+                            <v-col class="d-flex text-h6">
+                                Spanish Email
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field
+                                    v-model="entryFormEmail.emailSubjectSpanish"
+                                    label="Subject Header" 
+                                    hide-details
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <ckeditor :editor="editor" v-model="entryFormEmail.emailContentSpanish" :config="editorConfig" @ready="onEditorSpanishReady"></ckeditor>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-btn @click="addStudentNameSpanish">Add Student Name</v-btn>
+                            </v-col>
+                        </v-row>
+                </v-col>
+            </v-row>
+            </div>       
+        </div>
         <v-row>
-            <v-col cols="6">
-                <v-text-field
-                    v-model="entryFormEmail.firstEmailDays"
-                    label="First Email Days"
-                    type="number"
-                    min="0"
-                ></v-text-field>
-            </v-col>
-            <v-col cols="6">
-                <v-text-field
-                    v-model="entryFormEmail.secondEmailDays"
-                    label="Second Email Days"
-                    type="number"
-                    min="0"
-                ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col class="d-flex text-h6">
-                Email
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="12">
-                <v-text-field 
-                    v-model="entryFormEmail.emailSubject"
-                    label="Subject Header" 
-                    hide-details
-                ></v-text-field>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col cols="12">
-                <ckeditor :editor="editor" v-model="editorData" :config="editorConfig" @ready="onEditorReady"></ckeditor>
+            <v-col class="d-flex justify-end">
+                <v-btn 
+                    @click="applyEmailConfigsConfirm = true"
+                    :disabled="emailDisabled"
+                >Apply</v-btn>
             </v-col>
         </v-row>
     </div>
 </v-container>
+<!-- Confirmation Dialog -->
+<v-dialog v-model="applyEmailConfigsConfirm" persistent max-width="500px">
+    <v-card>
+        <v-card-title class="text-h5">
+            Confirm Email
+        </v-card-title>
+        <v-card-text>
+            <p>Are you sure you want to apply this configuration?</p>
+        </v-card-text>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="cancelEmailConfigs">
+                Cancel
+            </v-btn>
+            <v-btn color="red darken-2" text @click="applyEmailConfigs">
+                Yes
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</v-dialog>
+entryFormEmail:
+<br><br>
+{{ entryFormEmail }}
 </template>
 
 <script>
@@ -67,26 +173,36 @@ import axios from 'axios';
 import { useLoggedInUserStore } from "@/stored/loggedInUser";
 import CKEditor from '@ckeditor/ckeditor5-vue';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { toast } from 'vue3-toastify';
 
 export default {
 name: "InstructorAutomaticMailerEntryForms",
 components: {
     ckeditor: CKEditor.component
 },
+inject: ['setTab'],
 data() {
     return {
         entryFormEmail: {
+            type: 'entryForm',
             enabled: false,
             firstEmailDays: 7,
             secondEmailDays: 14,
+            multilingual: false,
             emailSubject: 'Reminder to Complete Your Entry Form',
+            emailSubjectEnglish: 'Email Subject English',
+            emailSubjectSpanish: 'Email Subject Spanish',
             emailContent: 'Please complete your entry form',
+            emailContentEnglish: 'Email Content English',
+            emailContentSpanish: 'Email Content Spanish',
         },
         configLoading: false,
         editorInstance: null,
+        editorEnglishInstance: null,
+        editorSpanishInstance: null,
         editor: ClassicEditor,
-        editorData: 'Please complete your Entry Form.',
         editorConfig: {},
+        applyEmailConfigsConfirm: false,
     };
 },
 created() {
@@ -95,6 +211,28 @@ created() {
 computed: {
     entryFormEmailStatus() {
         return this.entryFormEmail.enabled ? 'Enabled' : 'Disabled';
+    },
+
+    emailDisabled() {
+        // Check if firstEmailDays and secondEmailDays are less than 1
+        const emailDaysInvalid = this.entryFormEmail.firstEmailDays < 1 || this.entryFormEmail.secondEmailDays < 1;
+
+        // If multilingual is true, check that all relevant fields are not empty
+        const multilingualFieldsInvalid = this.entryFormEmail.multilingual && (
+            !this.entryFormEmail.emailSubjectEnglish ||
+            !this.entryFormEmail.emailSubjectSpanish ||
+            !this.entryFormEmail.emailContentEnglish ||
+            !this.entryFormEmail.emailContentSpanish
+        );
+
+        // If multilingual is false, check that the non-language-specific fields are not empty
+        const nonMultilingualFieldsInvalid = !this.entryFormEmail.multilingual && (
+            !this.entryFormEmail.emailSubject ||
+            !this.entryFormEmail.emailContent
+        );
+
+        // The button should be disabled if any of the above conditions are true
+        return emailDaysInvalid || multilingualFieldsInvalid || nonMultilingualFieldsInvalid;
     }
 },
 methods: {
@@ -109,7 +247,12 @@ methods: {
         try {
             const response = await axios.get(url, { headers: { token } });
             console.log('response: ', response);
-            this.entryFormEmail = response.data;
+            // Create a new object excluding the _id field
+            const { _id, ...configWithoutId } = response.data;
+            
+            // Assign the modified config to your component's state
+            this.entryFormEmail = configWithoutId;
+
         } catch (error) {
             this.handleError(error);
         } finally {
@@ -120,6 +263,76 @@ methods: {
     onEditorReady(editorInstance) {
         this.editorInstance = editorInstance;
     },
+
+    onEditorEnglishReady(editorInstance) {
+        this.editorEnglishInstance = editorInstance;
+    },
+
+    onEditorSpanishReady(editorInstance) {
+        this.editorSpanishInstance = editorInstance;
+    },
+
+    addStudentName() {
+        const editor = this.editorInstance;
+        if (editor) {
+            editor.model.change(writer => {
+                const placeholderText = writer.createText('{{STUDENT_NAME}}');
+                const insertPosition = editor.model.document.selection.getFirstPosition();
+                editor.model.insertContent(placeholderText, insertPosition);
+            });
+        }
+    },
+
+    addStudentNameEnglish() {
+        const editor = this.editorEnglishInstance;
+        if (editor) {
+            editor.model.change(writer => {
+                const placeholderText = writer.createText('{{STUDENT_NAME}}');
+                const insertPosition = editor.model.document.selection.getFirstPosition();
+                editor.model.insertContent(placeholderText, insertPosition);
+            });
+        }
+    },
+
+    addStudentNameSpanish() {
+        const editor = this.editorSpanishInstance;
+        if (editor) {
+            editor.model.change(writer => {
+                const placeholderText = writer.createText('{{STUDENT_NAME}}');
+                const insertPosition = editor.model.document.selection.getFirstPosition();
+                editor.model.insertContent(placeholderText, insertPosition);
+            });
+        }
+    },
+
+    cancelEmailConfigs() {
+        this.applyEmailConfigsConfirm = false;
+    },
+
+    applyEmailConfigs() {
+        const user = useLoggedInUserStore();
+        // const token = user.token;
+        const token = import.meta.env.VITE_TOKEN;
+        let apiURL = `${import.meta.env.VITE_ROOT_API}/instructorSideData/auto-mailer-config/entryForm`;
+        try {
+            const response = axios.put(apiURL, this.entryFormEmail, { headers: { token }});
+            console.log('Configuration updated successfully', response.data);
+
+            // Display the toast message
+            toast.info('Entry Form Automatic Mailer Updated!', {
+                position: "top-right",
+                toastClassName: "Toastify__toast--update",
+                multiple: false
+            });
+
+            this.setTab('overview');
+
+        } catch (error) {
+            this.handleError(error);
+        } finally {
+            this.applyEmailConfigsConfirm = false;
+        }
+    }
 }
 }
 </script>
--- src/components/instructorSide/instructorMailer/instructorAutomaticMailer/instructorAutomaticMailerGoalForms.vue
+++ src/components/instructorSide/instructorMailer/instructorAutomaticMailer/instructorAutomaticMailerGoalForms.vue
@@ -1,11 +1,381 @@
 <template>
-Goal Forms
+<v-container>
+    <div v-if="configLoading">
+        <v-row>
+            <v-col class="d-flex justify-center">
+                <v-progress-circular indeterminate></v-progress-circular>
+            </v-col>
+        </v-row>
+    </div>
+    <div v-else>
+        <v-row>
+            <v-col cols="12" class="d-flex text-h6">
+                <h1>Entry Form Email Configuration</h1>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-switch
+                    v-model="goalSettingFormEmail.enabled"
+                    :label="goalSettingFormEmailStatus"
+                ></v-switch>
+            </v-col>
+        </v-row>
+        <div v-if="goalSettingFormEmail.enabled">
+            <v-row>
+                <v-col cols="6">
+                    <v-text-field
+                        v-model="goalSettingFormEmail.firstEmailDays"
+                        label="First Email Days"
+                        type="number"
+                        min="0"
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="6">
+                    <v-text-field
+                        v-model="goalSettingFormEmail.secondEmailDays"
+                        label="Second Email Days"
+                        type="number"
+                        min="0"
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-checkbox
+                        v-model="goalSettingFormEmail.multilingual"
+                        label="Include Translation"
+                        hide-details    
+                    ></v-checkbox>
+                    <div class="text-subtitle-2 ml-4">Write a message in English and Spanish. Students will receive one based on their "Language Preference"</div>
+                </v-col>
+            </v-row>
+            <div v-if="!goalSettingFormEmail.multilingual">
+                <v-row>
+                    <v-col class="d-flex text-h6">
+                        Email
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12">
+                        <v-text-field 
+                            v-model="goalSettingFormEmail.emailSubject"
+                            label="Subject Header" 
+                            hide-details
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12">
+                        <ckeditor :editor="editor" v-model="goalSettingFormEmail.emailContent" :config="editorConfig" @ready="onEditorReady"></ckeditor>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="6">
+                        <v-btn @click="addStudentName">Add Student Name</v-btn>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-btn @click="addExperienceName">Add Experience Name</v-btn>
+                    </v-col>
+                </v-row>
+            </div>
+            <div v-else>
+                <v-row>
+                    <v-col cols="6">
+                        <v-row>
+                            <v-col class="d-flex text-h6">
+                                English Email
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field 
+                                    v-model="goalSettingFormEmail.emailSubjectEnglish"
+                                    label="Subject Header" 
+                                    hide-details
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <ckeditor :editor="editor" v-model="goalSettingFormEmail.emailContentEnglish" :config="editorConfig" @ready="onEditorEnglishReady"></ckeditor>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="6">
+                                <v-btn @click="addStudentNameEnglish">Add Student Name</v-btn>
+                            </v-col>
+                            <v-col cols="6">
+                                <v-btn @click="addExperienceNameEnglish">Add Experience Name</v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                    <v-col cols="6">
+                        <v-row>
+                            <v-col class="d-flex text-h6">
+                                Spanish Email
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-text-field
+                                    v-model="goalSettingFormEmail.emailSubjectSpanish"
+                                    label="Subject Header" 
+                                    hide-details
+                                ></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                                <ckeditor :editor="editor" v-model="goalSettingFormEmail.emailContentSpanish" :config="editorConfig" @ready="onEditorSpanishReady"></ckeditor>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="6">
+                                <v-btn @click="addStudentNameSpanish">Add Student Name</v-btn>
+                            </v-col>
+                            <v-col cols="6">
+                                <v-btn @click="addExperienceNameSpanish">Add Experience Name</v-btn>
+                            </v-col>
+                        </v-row>
+                </v-col>
+            </v-row>
+            </div>       
+        </div>
+        <v-row>
+            <v-col class="d-flex justify-end">
+                <v-btn 
+                    @click="applyEmailConfigsConfirm = true"
+                    :disabled="emailDisabled"
+                >Apply</v-btn>
+            </v-col>
+        </v-row>
+    </div>
+</v-container>
+<!-- Confirmation Dialog -->
+<v-dialog v-model="applyEmailConfigsConfirm" persistent max-width="500px">
+    <v-card>
+        <v-card-title class="text-h5">
+            Confirm Email
+        </v-card-title>
+        <v-card-text>
+            <p>Are you sure you want to apply this configuration?</p>
+        </v-card-text>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text @click="cancelEmailConfigs">
+                Cancel
+            </v-btn>
+            <v-btn color="red darken-2" text @click="applyEmailConfigs">
+                Yes
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</v-dialog>
+goalSettingFormEmail:
+<br><br>
+{{ goalSettingFormEmail }}
 </template>
 
 <script>
+import axios from 'axios';
+import { useLoggedInUserStore } from "@/stored/loggedInUser";
+import CKEditor from '@ckeditor/ckeditor5-vue';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
+export default {
+name: "InstructorAutomaticMailerGoalForms",
+components: {
+    ckeditor: CKEditor.component
+},
+data() {
+    return {
+        goalSettingFormEmail: {
+            type: 'goalSettingForm',
+            enabled: false,
+            firstEmailDays: null,
+            secondEmailDays: null,
+            multilingual: false,
+            emailSubject: 'Reminder to Complete Your Entry Form',
+            emailSubjectEnglish: 'Email Subject English',
+            emailSubjectSpanish: 'Email Subject Spanish',
+            emailContent: 'Please complete your entry form',
+            emailContentEnglish: 'Email Content English',
+            emailContentSpanish: 'Email Content Spanish',
+        },
+        configLoading: false,
+        editorInstance: null,
+        editorEnglishInstance: null,
+        editorSpanishInstance: null,
+        editor: ClassicEditor,
+        editorConfig: {},
+        applyEmailConfigsConfirm: false,
+    };
+},
+created() {
+    this.fetchAutoMailerConfigGoalSettingForms();
+},
+computed: {
+    goalSettingFormEmailStatus() {
+        return this.goalSettingFormEmail.enabled ? 'Enabled' : 'Disabled';
+    },
+
+    emailDisabled() {
+        // Check if firstEmailDays and secondEmailDays are less than 1
+        const emailDaysInvalid = this.goalSettingFormEmail.firstEmailDays < 1 || this.goalSettingFormEmail.secondEmailDays < 1;
+
+        // If multilingual is true, check that all relevant fields are not empty
+        const multilingualFieldsInvalid = this.goalSettingFormEmail.multilingual && (
+            !this.goalSettingFormEmail.emailSubjectEnglish ||
+            !this.goalSettingFormEmail.emailSubjectSpanish ||
+            !this.goalSettingFormEmail.emailContentEnglish ||
+            !this.goalSettingFormEmail.emailContentSpanish
+        );
+
+        // If multilingual is false, check that the non-language-specific fields are not empty
+        const nonMultilingualFieldsInvalid = !this.goalSettingFormEmail.multilingual && (
+            !this.goalSettingFormEmail.emailSubject ||
+            !this.goalSettingFormEmail.emailContent
+        );
+
+        // The button should be disabled if any of the above conditions are true
+        return emailDaysInvalid || multilingualFieldsInvalid || nonMultilingualFieldsInvalid;
+    }
+},
+methods: {
+    async fetchAutoMailerConfigGoalSettingForms() {
+        this.configLoading = true;
+        const user = useLoggedInUserStore();
+        // const token = user.token;
+        const token = import.meta.env.VITE_TOKEN;
+        const type = 'goalSettingForm';
+        let url = import.meta.env.VITE_ROOT_API + `/instructorSideData/auto-mailer-config/${type}`;
+
+        try {
+            const response = await axios.get(url, { headers: { token } });
+            console.log('response: ', response);
+            // Create a new object excluding the _id field
+            const { _id, ...configWithoutId } = response.data;
+            
+            // Assign the modified config to your component's state
+            this.goalSettingFormEmail = configWithoutId;
+
+        } catch (error) {
+            this.handleError(error);
+        } finally {
+            this.configLoading = false;
+        }
+    },
+
+    onEditorReady(editorInstance) {
+        this.editorInstance = editorInstance;
+    },
+
+    onEditorEnglishReady(editorInstance) {
+        this.editorEnglishInstance = editorInstance;
+    },
+
+    onEditorSpanishReady(editorInstance) {
+        this.editorSpanishInstance = editorInstance;
+    },
+
+    addStudentName() {
+        const editor = this.editorInstance;
+        if (editor) {
+            editor.model.change(writer => {
+                const placeholderText = writer.createText('{{STUDENT_NAME}}');
+                const insertPosition = editor.model.document.selection.getFirstPosition();
+                editor.model.insertContent(placeholderText, insertPosition);
+            });
+        }
+    },
+
+    addStudentNameEnglish() {
+        const editor = this.editorEnglishInstance;
+        if (editor) {
+            editor.model.change(writer => {
+                const placeholderText = writer.createText('{{STUDENT_NAME}}');
+                const insertPosition = editor.model.document.selection.getFirstPosition();
+                editor.model.insertContent(placeholderText, insertPosition);
+            });
+        }
+    },
+
+    addStudentNameSpanish() {
+        const editor = this.editorSpanishInstance;
+        if (editor) {
+            editor.model.change(writer => {
+                const placeholderText = writer.createText('{{STUDENT_NAME}}');
+                const insertPosition = editor.model.document.selection.getFirstPosition();
+                editor.model.insertContent(placeholderText, insertPosition);
+            });
+        }
+    },
+
+    addExperienceName() {
+        const editor = this.editorInstance;
+        if (editor) {
+            editor.model.change(writer => {
+                const placeholderText = writer.createText('{{EXPERIENCE_NAME}}');
+                const insertPosition = editor.model.document.selection.getFirstPosition();
+                editor.model.insertContent(placeholderText, insertPosition);
+            });
+        }
+    },
+
+    addExperienceNameEnglish() {
+        const editor = this.editorEnglishInstance;
+        if (editor) {
+            editor.model.change(writer => {
+                const placeholderText = writer.createText('{{EXPERIENCE_NAME}}');
+                const insertPosition = editor.model.document.selection.getFirstPosition();
+                editor.model.insertContent(placeholderText, insertPosition);
+            });
+        }
+    },
+
+    addExperienceNameSpanish() {
+        const editor = this.editorSpanishInstance;
+        if (editor) {
+            editor.model.change(writer => {
+                const placeholderText = writer.createText('{{EXPERIENCE_NAME}}');
+                const insertPosition = editor.model.document.selection.getFirstPosition();
+                editor.model.insertContent(placeholderText, insertPosition);
+            });
+        }
+    },
+
+    
+
+    cancelEmailConfigs() {
+        this.applyEmailConfigsConfirm = false;
+    },
+
+    applyEmailConfigs() {
+        const user = useLoggedInUserStore();
+        // const token = user.token;
+        const token = import.meta.env.VITE_TOKEN;
+        let apiURL = `${import.meta.env.VITE_ROOT_API}/instructorSideData/auto-mailer-config/goalSettingForm`;
+        try {
+            const response = axios.put(apiURL, this.goalSettingFormEmail, { headers: { token }});
+            
+            // Display the toast message
+            toast.info('Goal Form Automatic Mailer Updated!', {
+                position: "top-right",
+                toastClassName: "Toastify__toast--update",
+                multiple: false
+            });
+
+            this.setTab('overview');
+        } catch (error) {
+            this.handleError(error);
+        } finally {
+            this.applyEmailConfigsConfirm = false;
+        }
+    }
+}
+}
 </script>
 
 <style scoped>
-
 </style>
